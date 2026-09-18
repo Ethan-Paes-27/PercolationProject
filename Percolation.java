01c7528ce3411772494d2482e434d083a3ee6b49
Ethan-Paes-27 <epaes27@jesuitmail.org>
2026-09-18 13:32:14 -0700

    ye

--- Percolation.java
+++ Percolation.java
@@ -1,9 +1,28 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private double n;
 
-
-    public static void main(String[] args) {
-
+// creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        
     }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col)
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col)
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col)
+
+    // returns the number of open sites
+    public int numberOfOpenSites()
+
+    // does the system percolate?
+    public boolean percolates()
+
+    // test client (optional)
+    public static void main(String[] args)
 }
